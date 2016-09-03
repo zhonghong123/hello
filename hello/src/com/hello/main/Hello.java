44cb6709cc1b6a64f584e6ba253317a4602f45db
xiezhognhong <18825156513@163.com>
2016-09-03 16:09:28 +0800

    aaaaa

---
+++ hello/src/com/hello/main/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) LiveV  Technologies(China),Inc.
+ */
+package com.hello.main;
+
+/**
+ * @name: Hello.java
+ * @description:
+ * <br><br>
+ * @author xiezhonghong
+ * <br><br>
+ * @date 2016年9月3日下午4:08:46
+ * @version since1.0
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello java");
+	}
+}
